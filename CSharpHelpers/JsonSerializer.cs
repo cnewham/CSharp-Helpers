b62a7e2c7081f1c0918426597d2d39d5f6731b08
Chris Newham <c.j.newham@gmail.com>
2012-09-27 10:45:42 -0400

    Added JsonSerializer

---
+++ CSharpHelpers/JsonSerializer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace CSharpHelpers
+{
+    public class JsonSerializer
+    {
+        /// <summary>
+        /// Builds JSON string from key-value pair and deserializes to a type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public T Deserialize<T>(Dictionary<string, string> values)
+        {
+            var serializer = new JavaScriptSerializer();
+            var jsonString = new StringBuilder("{");
+
+            foreach (KeyValuePair<string,string> value in values)
+            {
+                jsonString.Append(this.FormatValuePair(value));
+            }
+
+            //Remove the last comma and close the json
+            jsonString.Replace(",", "", jsonString.Length - 1, 1);
+            jsonString.Append("}");
+
+            string temp = jsonString.ToString();
+            return serializer.Deserialize<T>(temp);
+        }
+
+        /// <summary>
+        /// Deserialize a well-formed JSON string to a type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public T Deserialize<T>(string jsonString)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<T>(jsonString);
+        }
+
+        /// <summary>
+        /// Formats key-value pair to well-formed JSON
+        /// </summary>
+        /// <param name="valuePair"></param>
+        /// <returns></returns>
+        private string FormatValuePair(KeyValuePair<string, string> valuePair )
+        {
+            //TODO: Cleanup illegal characters
+
+            return string.Format("\"{0}\":\"{1}\",", valuePair.Key, valuePair.Value);
+        }
+
+    }
+}
