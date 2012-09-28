468004c436ad39b852bab1b52878bf86d860ff87
Chris Newham <c.j.newham@gmail.com>
2012-09-28 08:44:35 -0400

    Added FlatFileHelper and XmlSerializer (incomplete)

---
+++ CSharpHelpers/FlatFileHelper.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpHelpers
+{
+    public class FlatFileHelper
+    {
+        #region Constructor
+        public FlatFileHelper(string FileName)
+        {
+            this.FileName = FileName;
+            this.Bytes = 0;
+
+            this.GetFileSize();
+        }
+        #endregion
+
+        #region Properties
+        public string FileName { get; set; }
+        public int Bytes { get; set; }
+
+        public char Delimiter { get; set; }
+        public bool RemoveQuotes { get; set; }
+        public bool HasHeaders { get; set; }
+
+        private long _Size;
+        public long Size
+        {
+            get
+            {
+                return _Size;
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private void GetFileSize()
+        {
+            FileInfo File = new FileInfo(this.FileName);
+
+            this._Size = File.Length;
+        }
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Reads the file until the end
+        /// </summary>
+        /// <returns></returns>
+        public List<string[]> ReadFile()
+        {
+            List<string[]> table = new List<string[]>();
+
+            using (StreamReader stream = new StreamReader(this.FileName))
+            {
+                string line;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] row;
+                    if (this.RemoveQuotes) { line.Replace("\"", ""); }
+
+                    row = line.Split(this.Delimiter);
+
+                    table.Add(row);
+                }
+
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Read line by line until EOF. Returns single line of data
+        /// </summary>
+        /// <returns></returns>
+        public string[] ReadLine()
+        {
+            string[] result;
+
+            using (StreamReader stream = new StreamReader(this.FileName))
+            {
+
+                // check HasHeaders flag and discard first line
+                if (HasHeaders && this.Bytes == 0)
+                {
+                    this.Bytes = stream.ReadLine().Length + 2;
+                }
+
+                char[] buffer = new char[this.Bytes];
+                stream.Read(buffer, 0, this.Bytes);
+
+                string line;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    if (this.RemoveQuotes) { line.Replace("\"", ""); }
+
+                    result = line.Split(this.Delimiter);
+
+                    if (line != null)
+                    {
+                        this.Bytes += line.Length + 2;
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ CSharpHelpers/XmlSerializer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace CSharpHelpers
+{
+    static class XmlSerializer
+    {
+
+        public static T DeserializeXml<T>(string xmlString)
+        {
+            //TODO: Implement Deserialize method
+            throw new NotImplementedException();
+        }
+
+        public static string SerializeXml<T>(T obj)
+        {
+            //Serialize the object
+            System.Xml.Serialization.XmlSerializer xmlize = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream();
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+
+            xmlize.Serialize(xmlTextWriter, obj);
+
+            memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+
+            string XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray());
+
+            //Close all the memory objects
+            xmlTextWriter.Close();
+            //xmlTextWriter.Flush();
+            memoryStream.Close();
+            memoryStream.Dispose();
+
+            //replace the tab charecter
+            XmlizedString = XmlizedString.Trim();
+            ﻿XmlizedString = XmlizedString.Replace("﻿", "");
+
+             return XmlizedString;
+        }
+
+        private static String UTF8ByteArrayToString(Byte[] characters)
+        {
+
+            UTF8Encoding encoding = new UTF8Encoding();
+            String constructedString = encoding.GetString(characters);
+            return (constructedString);
+        }
+
+        private static Byte[] StringToUTF8ByteArray(String pXmlString)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            Byte[] byteArray = encoding.GetBytes(pXmlString);
+            return byteArray;
+        }
+    }
+}
